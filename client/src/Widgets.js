cb9c6f71b3e5dbf412786b36f56fd3991904cf37
shauryat <43247773+shauryat@users.noreply.github.com>
2022-12-20 18:19:16 +0530

    first commit

---
+++ client/src/Widgets.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import Card from "@material-ui/core/Card";
+
+import "./Widgets.css";
+
+function Widgets() {
+  return (
+    <div className="widgets">
+      {/* <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div> */}
+
+      <div className="widgets__widgetContainer">
+        <h2>Our Sponsors</h2>
+      </div>
+      <div className="sponsor_cont">
+        <Card variant="outlined">
+          <img
+            src="https://download.logo.wine/logo/Ethereum/Ethereum-Logo.wine.png"
+            width="300"
+            height="200"
+          />
+        </Card>
+        <br />
+        <Card variant="outlined">
+          <img
+            src="https://assets-global.website-files.com/5f973c970bea5548ad4287ef/6088f4c7c34ad61ab10cdf72_horizontal-logo-onecolor-neutral-alchemy.svg"
+            width="300"
+            height="65"
+            style={{ margin: "15px" }}
+          />
+        </Card>
+        <br />
+
+        <Card variant="outlined">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/c/c2/IPFS_logo.png"
+            width="300"
+            height="121"
+            style={{ margin: "15px" }}
+          />
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default Widgets;
